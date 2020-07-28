f14c04ccc85c693047e6ce4b9021f7206a0b81e9
dmitriy.kononov <kononov.d.p@gmail.com>
2020-07-28 13:12:04 +0300

    header - desktop markup

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
 
-	// Custom JS
+  $('.dropdown__header').on('click', function(e) {
+    e.preventDefault();
+    let ariaExp = $('.dropdown__header').attr('aria-expanded');
 
-});
+    $(this).closest('.dropdown').toggleClass('dropdown--active');
+
+    if($(this).attr('aria-expanded') === 'true') {
+      $(this).attr('aria-expanded', false);
+    }else {
+      $(this).attr('aria-expanded', true)
+    }
+
+  })
+
+})
